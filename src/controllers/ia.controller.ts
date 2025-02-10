4e1d28703ba065d7f723d001150994c8ca58fa71
Diego Jimenez <diego.jimenez@alaya.cl>
2025-02-09 21:22:06 -0300

    fix: :ambulance: fix chat

--- src/controllers/ia.controller.ts
+++ src/controllers/ia.controller.ts
@@ -11,12 +11,17 @@ import { donwloadFileServices } from "../services/donwloadFile.services";
 import { createThread, messageThread } from "../services/openai.services";
 
 // Download File from IA
-export const downloadFileIA = async (req: Request, res: Response, next: any): Promise<any> => {
+export const downloadFileIA = async (
+  req: Request,
+  res: Response,
+  next: any
+): Promise<any> => {
   try {
     const { idFile } = req.params;
 
     // Retrieve token
-    const Authorization = req.get("Authorization") || req.query.token || req.body.token;
+    const Authorization =
+      req.get("Authorization") || req.query.token || req.body.token;
     const tokenparts = splitTokenParts(Authorization);
 
     // Download file content and retrive mimetypes
@@ -34,8 +39,11 @@ export const downloadFileIA = async (req: Request, res: Response, next: any): Pr
   }
 };
 
-// Chat with assistant
-export const chatAssistant = async (req: Request, res: Response, next: any): Promise<any> => {
+export const chatAssistant = async (
+  req: Request,
+  res: Response,
+  next: any
+): Promise<any> => {
   try {
     // request data
     const body = req.body;
@@ -46,12 +54,20 @@ export const chatAssistant = async (req: Request, res: Response, next: any): Pro
     res.flushHeaders();
 
     // Credentiales User
-    const Authorization = req.get("Authorization") || req.query.token || req.body.token;
-    const infoUser: any = decodedToken(Authorization);
-    const id_usuario = infoUser?.payload?.id_usuario;
-    const credentials = await userCredentialsAi(id_usuario);
+    const Authorization =
+      req.get("Authorization") || req.query.token || req.body.token;
+    const tokenparts = splitTokenParts(Authorization);
+    const dataFromToken: any = decodedAnyToken(Authorization);
 
-    if (!credentials) {
+    const user_data = await retrieveUserWithEmail(
+      dataFromToken?.payload?.preferred_username.toLowerCase()
+    );
+    const id_usuario = user_data?.id_usuario;
+
+    const credentials = await areaCredentialsAi(id_usuario as string);
+    const credentialsUser = await userCredentialsAi(id_usuario as string);
+
+    if (!credentials || !credentialsUser) {
       logger.error({ err: "No se encuentran las credenciales del usuario" });
       return next(
         createError(500, {
@@ -62,23 +78,28 @@ export const chatAssistant = async (req: Request, res: Response, next: any): Pro
 
     // IA parameters
     const assistantsClient = getClient();
-    const assistantId = credentials.asistente;
-    const vectorStore = credentials.vectores;
+    const assistantId = body[body.length - 1]?.closeFiles
+      ? credentials.asistente
+      : credentialsUser.asistente;
+    const vectorStore = body[body.length - 1]?.closeFiles
+      ? credentials.vectores
+      : credentialsUser.vectores;
 
-    const content = body[body?.length - 1]?.content;
+    const content = body[body.length - 1]?.content;
     let text = "";
     const objectResponse = {
       response: "",
       id_file_download: "",
-      thread_id_openai: body[body?.length - 1]?.threadId,
+      thread_id_openai: body[body.length - 1]?.threadId,
       id_image_download: "",
     };
 
     // Create attachment for code_interpreter and file list for system prompt
-    const attachmentsCode: MessageCreateParams.Attachment[] | null | undefined = [];
+    const attachmentsCode: MessageCreateParams.Attachment[] | null | undefined =
+      [];
     const attachmentsSearch: string[] = [];
-    if (body[body?.length - 1]?.filesCodeInterpreter?.length > 0) {
-      const idFiles = body[body?.length - 1]?.filesCodeInterpreter;
+    if (body[body.length - 1]?.filesCodeInterpreter.length > 0) {
+      const idFiles = body[body.length - 1]?.filesCodeInterpreter;
       idFiles.forEach((idFile: string) => {
         attachmentsCode.push({
           file_id: idFile,
@@ -87,16 +108,14 @@ export const chatAssistant = async (req: Request, res: Response, next: any): Pro
       });
     }
 
-    if (body[body?.length - 1]?.fileSearch.length > 0) {
-      const idFiles = body[body?.length - 1]?.fileSearch;
+    if (body[body.length - 1]?.fileSeach.length > 0) {
+      const idFiles = body[body.length - 1]?.fileSeach;
       idFiles.forEach((fileName: string) => {
         attachmentsSearch.push(fileName);
       });
     }
 
-    const instruction = `Como gestor de conocimiento te haré preguntas, usa tu File search o tu Code interpreter para abrir y leer los archivos proporcionados, cuando sea necesario.
-        Eres Thinker, la Inteligencia Artificial Generativa de Alaya, un asistente virtual que te ayudará a encontrar información relevante en documentos y fuentes de conocimiento.
-        Cumple con las siguientes instrucciones para generar tu respuesta:
+    const instruction = `Como gestor de conocimiento te haré preguntas, usa tu File search o tu Code interpreter para abrir y leer los archivos proporcionados
         1. Adicionalmente, agrega análisis, razonamientos y justificaciones de tu respuesta.
         2. Tu respuesta debe incluir la mayor cantidad de información que pueda estar relacionada con la pregunta.
         3. Escribe tu respuesta de forma estructurada e incluye listados si es necesario
@@ -113,68 +132,74 @@ export const chatAssistant = async (req: Request, res: Response, next: any): Pro
 
     // 1. Start a new thread
     if (objectResponse.thread_id_openai === "") {
-      const threadIdResponse = await createThread(vectorStore, assistantsClient);
+      const threadIdResponse = await createThread(
+        tokenparts,
+        vectorStore,
+        assistantsClient
+      );
       objectResponse.thread_id_openai = threadIdResponse;
     }
 
     // 2. add message to thread
-    await messageThread(assistantsClient, objectResponse.thread_id_openai, attachmentsCode, content);
+    await messageThread(
+      assistantsClient,
+      objectResponse.thread_id_openai,
+      attachmentsCode,
+      content
+    );
 
     // 3. Run stream
-    const run = assistantsClient.beta.threads.runs
+    const stream = assistantsClient.beta.threads.runs
       .stream(objectResponse.thread_id_openai, {
         assistant_id: assistantId,
-        stream: true,
-        temperature: 0.03,
-        top_p: 1,
-        model: "gpt-4o",
-        instructions: instruction,
       })
-      .on("textCreated", (text) => process.stdout.write("\ntextCreated assistant > "))
+      .on("textCreated", () => console.log("assistant >"))
+      .on("toolCallCreated", (event) => console.log("assistant " + event.type))
       .on("textDelta", (textDelta: any) => {
+        console.log("Received textDelta:", textDelta);
         if (textDelta && typeof textDelta.value === "string") {
           text += textDelta.value;
           objectResponse.response = text;
-        } else {
-          // retrieve id_file generated
-          objectResponse.id_file_download = textDelta.annotations[0].file_path.file_id;
+        } else if (textDelta.annotations && textDelta.annotations.length > 0) {
+          objectResponse.id_file_download =
+            textDelta.annotations[0].file_path.file_id;
         }
-        // process.stdout.write(text);
         res.write(JSON.stringify(objectResponse));
       })
-      .on("toolCallCreated", (toolCall) => process.stdout.write(`\ntoolCallCreated assistant >`))
-      .on("toolCallDelta", async (toolCallDelta: any, snapshot) => {
-        // Get file image id and send in response
-        const file_id = toolCallDelta?.code_interpreter?.outputs ? toolCallDelta?.code_interpreter?.outputs[0]?.image?.file_id : "";
-        let base64 = "";
-        objectResponse.id_image_download = "base64 empty";
-        if (file_id) {
-          logger.info(`File image id generated: ID ${file_id}`);
-          base64 = file_id;
-        }
-        objectResponse.id_image_download = base64;
-        res.write(JSON.stringify(objectResponse));
+      .on("messageDone", async (event) => {
+        if (event.content[0].type === "text") {
+          const { text: textDelta } = event.content[0];
+          const { annotations } = textDelta;
+          const citations: string[] = [];
 
-        if (toolCallDelta.type === "code_interpreter") {
-          if (toolCallDelta.code_interpreter.input) {
-            process.stdout.write("");
+          let index = 0;
+          for (let annotation of annotations) {
+            textDelta.value = textDelta.value.replace(
+              annotation.text,
+              "[" + index + "]"
+            );
+            const { file_citation }: any = annotation;
+            if (file_citation) {
+              const citedFile = await assistantsClient.files.retrieve(
+                file_citation.file_id
+              );
+              citations.push("[" + index + "]" + citedFile.filename);
+            }
+            index++;
           }
-          if (toolCallDelta.code_interpreter.outputs) {
-            process.stdout.write("\ntoolCallDelta output >\n");
-            toolCallDelta.code_interpreter.outputs.forEach((output: any) => {
-              if (output.type === "logs") {
-                process.stdout.write(``);
-              }
-            });
-          }
+
+          text += textDelta.value;
+          objectResponse.response = text;
+
+          console.log(objectResponse.response);
         }
       })
       .on("end", () => {
         res.end();
       });
 
-    // close connection
     req.on("close", () => {
+      logger.info("Client closed connection");
       res.end();
     });
   } catch (err: any) {
